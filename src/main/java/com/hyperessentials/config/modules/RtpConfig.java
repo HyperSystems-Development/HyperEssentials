2e1bbeec74a0d2ec0520286683198a1987a5a5f6
ZenithDevHQ <ZenithDevHQ@users.noreply.github.com>
2026-02-22 23:41:30 -0500

    feat: implement RTP module with random location finding and safety checks

--- src/main/java/com/hyperessentials/config/modules/RtpConfig.java
+++ src/main/java/com/hyperessentials/config/modules/RtpConfig.java
@@ -1,15 +1,62 @@
 package com.hyperessentials.config.modules;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import org.jetbrains.annotations.NotNull;
+
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.hyperessentials.config.ModuleConfig;
 
 public class RtpConfig extends ModuleConfig {
+    private int centerX = 0;
+    private int centerZ = 0;
+    private int minRadius = 100;
+    private int maxRadius = 5000;
+    private int maxAttempts = 10;
+    private List<String> blacklistedWorlds = new ArrayList<>();
+
     public RtpConfig(@NotNull Path filePath) { super(filePath); }
     @Override @NotNull public String getModuleName() { return "rtp"; }
     @Override protected boolean getDefaultEnabled() { return false; }
     @Override protected void createDefaults() {}
-    @Override protected void loadModuleSettings(@NotNull JsonObject root) {}
-    @Override protected void writeModuleSettings(@NotNull JsonObject root) {}
+
+    @Override protected void loadModuleSettings(@NotNull JsonObject root) {
+        centerX = getInt(root, "centerX", centerX);
+        centerZ = getInt(root, "centerZ", centerZ);
+        minRadius = getInt(root, "minRadius", minRadius);
+        maxRadius = getInt(root, "maxRadius", maxRadius);
+        maxAttempts = getInt(root, "maxAttempts", maxAttempts);
+
+        blacklistedWorlds = new ArrayList<>();
+        if (root.has("blacklistedWorlds") && root.get("blacklistedWorlds").isJsonArray()) {
+            JsonArray arr = root.getAsJsonArray("blacklistedWorlds");
+            for (int i = 0; i < arr.size(); i++) {
+                blacklistedWorlds.add(arr.get(i).getAsString().toLowerCase());
+            }
+        }
+    }
+
+    @Override protected void writeModuleSettings(@NotNull JsonObject root) {
+        root.addProperty("centerX", centerX);
+        root.addProperty("centerZ", centerZ);
+        root.addProperty("minRadius", minRadius);
+        root.addProperty("maxRadius", maxRadius);
+        root.addProperty("maxAttempts", maxAttempts);
+
+        JsonArray arr = new JsonArray();
+        for (String world : blacklistedWorlds) {
+            arr.add(world);
+        }
+        root.add("blacklistedWorlds", arr);
+    }
+
+    public int getCenterX() { return centerX; }
+    public int getCenterZ() { return centerZ; }
+    public int getMinRadius() { return minRadius; }
+    public int getMaxRadius() { return maxRadius; }
+    public int getMaxAttempts() { return maxAttempts; }
+    public List<String> getBlacklistedWorlds() { return blacklistedWorlds; }
 }
--- src/main/java/com/hyperessentials/module/rtp/RtpModule.java
+++ src/main/java/com/hyperessentials/module/rtp/RtpModule.java
@@ -2,7 +2,9 @@ package com.hyperessentials.module.rtp;
 
 import com.hyperessentials.config.ConfigManager;
 import com.hyperessentials.config.ModuleConfig;
+import com.hyperessentials.config.modules.RtpConfig;
 import com.hyperessentials.module.AbstractModule;
+import com.hyperessentials.util.Logger;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -10,6 +12,8 @@ import org.jetbrains.annotations.Nullable;
  * Random Teleport module for HyperEssentials.
  */
 public class RtpModule extends AbstractModule {
+
+    private RtpManager rtpManager;
 
     @Override
     @NotNull
@@ -26,13 +30,19 @@ public class RtpModule extends AbstractModule {
     @Override
     public void onEnable() {
         super.onEnable();
-        // TODO: Register commands, listeners, and storage
+        RtpConfig config = ConfigManager.get().rtp();
+        this.rtpManager = new RtpManager(config);
+        Logger.info("[RTP] RtpManager initialized");
     }
 
     @Override
     public void onDisable() {
-        // TODO: Unregister commands, save data, cleanup
         super.onDisable();
+    }
+
+    @Nullable
+    public RtpManager getRtpManager() {
+        return rtpManager;
     }
 
     @Override
